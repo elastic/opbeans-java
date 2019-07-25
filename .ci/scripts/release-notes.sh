cd88199c290cb1fbe339d2baf71cd2dda057f1f7
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-07-25 12:37:45 +0100

    ci: enable gren release notes

---
+++ .ci/scripts/release-notes.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -uxeo pipefail
+
+GREN_GITHUB_TOKEN=${GREN_GITHUB_TOKEN:?"missing GREN_GITHUB_TOKEN"}
+
+gren release --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all
