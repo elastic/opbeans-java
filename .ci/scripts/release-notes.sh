778d2d152bce61f78dceee286c43a2fd0fa3d561
Victor Martinez <victormartinezrubio@gmail.com>
2019-09-11 13:59:42 +0100

    speed up and debug (#1)

--- .ci/scripts/release-notes.sh
+++ .ci/scripts/release-notes.sh
@@ -3,4 +3,4 @@ set -uxeo pipefail
 
 GREN_GITHUB_TOKEN=${GREN_GITHUB_TOKEN:?"missing GREN_GITHUB_TOKEN"}
 
-gren release --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all
+gren release --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all --debug
