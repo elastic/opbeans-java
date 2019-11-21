1e3adfa76748934c32f48a46b32124f1fadb8a30
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-21 14:46:26 +0000

    Bump version

---
+++ .ci/bump-version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+AGENT_VERSION="${1:?"Please set the java agent version"}"
+
+# Update agent dependencies
+cd opbeans
+./mvnw -B versions:use-dep-version -DdepVersion=${AGENT_VERSION} -Dincludes=co.elastic.apm:apm-agent-api
+
+# Commit changes
+git add pom.xml
+git commit -m "Bump version ${AGENT_VERSION}"
