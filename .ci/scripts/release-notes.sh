203fae64d4c35aa659f5a9ed8b4bc7d1e90ad32b
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-11-21 14:24:35 +0000

    changelog is not yet supported

--- .ci/scripts/release-notes.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -uxeo pipefail
-
-GREN_GITHUB_TOKEN=${GREN_GITHUB_TOKEN:?"missing GREN_GITHUB_TOKEN"}
-
-gren release --token="${GREN_GITHUB_TOKEN}" --override -c .grenrc.js -t all --debug
