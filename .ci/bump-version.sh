3ef66293e40d4169db7c9b2632b21db23a45afbf
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-07-01 12:41:08 +0100

    docker: add labels metadata

--- .ci/bump-version.sh
+++ .ci/bump-version.sh
@@ -5,8 +5,11 @@ AGENT_VERSION="${1:?"Please set the java agent version"}"
 
 # Update agent dependencies
 cd opbeans
-./mvnw -B versions:use-dep-version -DdepVersion=${AGENT_VERSION} -Dincludes=co.elastic.apm:apm-agent-api
+./mvnw -B versions:use-dep-version -DdepVersion="${AGENT_VERSION}" -Dincludes=co.elastic.apm:apm-agent-api
+
+## Bump agent version in the Dockerfile
+sed -ibck "s#\(org.label-schema.version=\)\(.*\)#\1\"${AGENT_VERSION}\"#g" Dockerfile
 
 # Commit changes
-git add pom.xml
+git add pom.xml Dockerfile
 git commit -m "Bump version ${AGENT_VERSION}"
