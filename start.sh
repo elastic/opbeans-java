99918dcb0f873fcbb5407d979a3abc4098e42751
Jean-Louis Voiseux <48380853+jlvoiseux@users.noreply.github.com>
2022-02-10 17:27:24 +0100

    Refactor start.sh
    Co-authored-by: SylvainJuge <syl20j@gmail.com>

--- start.sh
+++ start.sh
@@ -1,31 +1,35 @@
 #!/bin/sh
 
-case $APM_AGENT_TYPE in
-  "opentelemetry")
-  echo "opentelemetry"
-  java -javaagent:/app/opentelemetry-javaagent.jar\
-                                        -Dspring.profiles.active=${OPBEANS_JAVA_PROFILE:-}\
-                                        -Dserver.port=${OPBEANS_SERVER_PORT:-}\
-                                        -Dserver.address=${OPBEANS_SERVER_ADDRESS:-0.0.0.0}\
-                                        -Dspring.datasource.url=${DATABASE_URL:-}\
-                                        -Dspring.datasource.driverClassName=${DATABASE_DRIVER:-}\
-                                        -Dspring.jpa.database=${DATABASE_DIALECT:-}\
-                                        -Dotel.instrumentation.runtime-metrics.enabled=true\
-                                        -jar ./app.jar
-  ;;
-  "elasticapm")
-  echo "elasticapm"
-  java -javaagent:/app/elastic-apm-agent.jar\
-                                        -Dspring.profiles.active=${OPBEANS_JAVA_PROFILE:-}\
-                                        -Dserver.port=${OPBEANS_SERVER_PORT:-}\
-                                        -Dserver.address=${OPBEANS_SERVER_ADDRESS:-0.0.0.0}\
-                                        -Dspring.datasource.url=${DATABASE_URL:-}\
-                                        -Dspring.datasource.driverClassName=${DATABASE_DRIVER:-}\
-                                        -Dspring.jpa.database=${DATABASE_DIALECT:-}\
-                                        -jar ./app.jar
-  ;;
-  *)
-  echo "unknown agent type $APM_AGENT_TYPE"
-  ;;
-  esac
-fi
+set -euo pipefail
+
+JAVA_AGENT=''
+
+APP_OPTS=""
+APP_OPTS="${APP_OPTS} -Dspring.profiles.active=${OPBEANS_JAVA_PROFILE:-}"
+APP_OPTS="${APP_OPTS} -Dserver.port=${OPBEANS_SERVER_PORT:-}"
+APP_OPTS="${APP_OPTS} -Dserver.address=${OPBEANS_SERVER_ADDRESS:-0.0.0.0}"
+APP_OPTS="${APP_OPTS} -Dspring.datasource.url=${DATABASE_URL:-}"
+APP_OPTS="${APP_OPTS} -Dspring.datasource.driverClassName=${DATABASE_DRIVER:-}"
+APP_OPTS="${APP_OPTS} -Dspring.jpa.database=${DATABASE_DIALECT:-}"
+
+case "${APM_AGENT_TYPE}" in
+    "opentelemetry")
+        echo "opentelemetry"
+        JAVA_AGENT="-javaagent:/app/opentelemetry-javaagent.jar"
+        APP_OPTS="${APP_OPTS} -Dotel.instrumentation.runtime-metrics.enabled=true"
+        ;;
+    "elasticapm")
+        echo "elasticapm"
+        JAVA_AGENT="-javaagent:/app/elastic-apm-agent.jar"
+        ;;
+    *)
+        echo "unknown agent type $APM_AGENT_TYPE"
+        exit 1
+        ;;
+esac
+
+java \
+    ${JAVA_AGENT} \
+    ${APP_OPTS} \
+    -jar ./app.jar
+
