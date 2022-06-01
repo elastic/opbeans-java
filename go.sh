5b810646a9624f872460f688e73a5a874a75d41c
Sylvain Juge <sylvain.juge@elastic.co>
2022-06-01 16:31:43 +0200

    update go.sh to use latest snapshot

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 java -version
 
@@ -13,14 +13,22 @@ export ELASTIC_APM_ENABLE_LOG_CORRELATION=true
 export ELASTIC_APM_ENABLE_EXPERIMENTAL_INSTRUMENTATIONS=true
 export ELASTIC_APM_ENVIRONMENT=debug
 
+# get released agent
 dl_agent_version=1.31.0
-curl -O https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${dl_agent_version}/elastic-apm-agent-${dl_agent_version}.jar
-agent_jar=elastic-apm-agent-${dl_agent_version}.jar
+curl -s -o elastic-apm-agent.jar "https://repo1.maven.org/maven2/co/elastic/apm/elastic-apm-agent/${dl_agent_version}/elastic-apm-agent-${dl_agent_version}.jar"
 
+# get latest snapshot
+curl -s -o elastic-apm-agent.jar --location 'https://apm-ci.elastic.co/job/apm-agent-java/job/apm-agent-java-mbp/job/main/lastSuccessfulBuild/artifact/src/github.com/elastic/apm-agent-java/elastic-apm-agent/target/elastic-apm-agent-1.31.1-SNAPSHOT.jar'
+
+agent_jar=elastic-apm-agent.jar
 app_jar=target/opbeans-0.0.1-SNAPSHOT.jar
+agent_opts=-javaagent:${agent_jar}
+
+#debug_opts='-agentlib:jdwp=transport=dt_socket,server=n,address=localhost:5005,suspend=y'
 
 java \
--javaagent:${agent_jar} \
+${debug_opts:-} \
+${agent_opts:-} \
 -jar ${app_jar}
 
 cd -
